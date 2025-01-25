42be21d001ed5ee5f36149551ebea29da7602e65
joangccantero <joan.gonzalez-condecantero@vistaprint.com>
2025-01-25 10:35:20 +0100

    :construction: Start integrating with existing scripts architecture

--- scripts/preprocess_graph_with_embeddings.py
+++ scripts/preprocess_graph_with_embeddings.py
@@ -7,15 +7,14 @@ from scripts.collate_fn import collate_fn
 def preprocess_graph_with_embeddings(input_folder_path, preprocessing_params):
     """Preprocessing pipeline for Graph NN."""
     print("Applying preprocessing for Graph NN with embeddings for categorical features...")
-    
-    embedding_model = NodeEmbedding(preprocessing_params.get("num_categories"),
-                                    preprocessing_params.get("num_sub_categories"),
-                                    preprocessing_params.get("num_elements"),
-                                    preprocessing_params.get("num_event_types"),
-                                    preprocessing_params.get("embedding_dim"))
 
     # Initialize dataset and dataloader
-    dataset = SessionGraphEmbeddingsDataset(input_folder_path, embedding_model)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=False, collate_fn=collate_fn)
-
+    dataset = SessionGraphEmbeddingsDataset(folder_path=input_folder_path,
+                                            start_month=preprocessing_params.get("start_month"),
+                                            end_month=preprocessing_params.get("end_month"),
+                                            transform=None,
+                                            test_sessions_first_n=preprocessing_params.get("test_sessions_first_n"), 
+                                            embedding_dim=64
+                                            )
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=preprocessing_params.get("shuffle"), collate_fn=collate_fn)
     return dataloader
