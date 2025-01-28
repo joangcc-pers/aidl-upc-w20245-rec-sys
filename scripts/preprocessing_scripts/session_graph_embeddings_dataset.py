6d0e1c913cf760914bd29b13def19bdb14e24386
joangccantero <joan.gonzalez-condecantero@vistaprint.com>
2025-01-28 22:04:17 +0100

    :construction: Refactor code to call embeddings properly

--- scripts/preprocessing_scripts/session_graph_embeddings_dataset.py
+++ scripts/preprocessing_scripts/session_graph_embeddings_dataset.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import torch
 
+import json
+
 from torch.utils.data import Dataset
 from torch_geometric.data import Data
 from sklearn.preprocessing import LabelEncoder
@@ -13,11 +15,12 @@ from scripts.preprocessing_scripts.node_embedding import NodeEmbedding
 class SessionGraphEmbeddingsDataset(Dataset):
     def __init__(self,
                  folder_path,
+                 output_folder_artifacts,
                  start_month,
                  end_month,
                  transform=None,
                  test_sessions_first_n=None, 
-                 embedding_dim=64,
+                #  embedding_dim=64,
                  limit_to_view_event=False,
                  drop_listwise_nulls=False
                  ):
@@ -25,6 +28,7 @@ class SessionGraphEmbeddingsDataset(Dataset):
         Dataset for session-based recommendation using Graph Neural Networks.
         Args:
             folder_path (str): Path to the folder containing session CSVs.
+            output_folder_artifacts (str): Path to the folder where the artifacts will be saved.
             start_month (str): Start month in 'YYYY-MM' format.
             end_month (str): End month in 'YYYY-MM' format.
             transform (callable, optional): Transform function for data augmentation.
@@ -36,7 +40,7 @@ class SessionGraphEmbeddingsDataset(Dataset):
 
 
         print("[INFO] Initializing SessionGraphDataset...")
-
+        
         # Step 1: Load CSV files
 
         # Validate and transform input dates using CsvFilesEnum
@@ -83,7 +87,17 @@ class SessionGraphEmbeddingsDataset(Dataset):
             self.data = self.data.dropna(subset=['brand', 'category_code', 'price']).reset_index(drop=True)
             print(f"[INFO] Data limited to {len(self.data)} rows with no nulls in 'brand', 'category_code' and 'price'.")
 
-        # Step 4 [Debugging]: Limit `self.data` to only the first k unique sessions
+        # TODO: quedarnos amb sessions que hi hagin 2 o més visualitzacions.
+        # self.data = self.data.groupby('user_session').filter(lambda x: len(x) > 1)
+        # Precompute the group sizes
+        group_sizes = self.data['user_session'].value_counts()
+
+        # Filter rows based on the group size
+        self.data = self.data[self.data['user_session'].isin(group_sizes[group_sizes > 1].index)]
+        self.data.reset_index(drop=True, inplace=True)
+        print(f"[INFO] Data limited to {len(self.data)} rows with 2 or more views in the session.")
+
+        # Step 4 [Debugging]: Limit `self.data` to only the first k unique sessions of the concatenated CSVs
 
         if test_sessions_first_n:
             print(f"[INFO:TEST_MODE] Limiting data to the first {str(test_sessions_first_n)} sessions...")
@@ -110,9 +124,6 @@ class SessionGraphEmbeddingsDataset(Dataset):
         # Debugging information
         print(self.data[['category', 'sub_category', 'element']])
 
-        
-        # ASSIGNAR UN ID A CADA CATEGORIA
-
         print("[INFO] Parsed category hierarchy into 'category', 'sub_category', and 'element' columns.")
 
         # Debugging information
@@ -132,6 +143,24 @@ class SessionGraphEmbeddingsDataset(Dataset):
         num_sub_categories = self.data['sub_category'].nunique()
         num_elements = self.data['element'].nunique()
         num_brands = self.data['brand'].nunique()
+        num_items = self.data['product_id'].nunique()
+
+        # Guardem en un JSON els valors únics de cada columna per a passar-los al NodeEmbedding
+        num_values_for_node_embedding = {
+            'num_categories': num_categories,
+            'num_sub_categories': num_sub_categories,
+            'num_elements': num_elements,
+            'num_brands': num_brands,
+            'num_items': num_items
+        }
+
+        #Create a new json file with the unique values of each column
+        os.makedirs(output_folder_artifacts, exist_ok=True)
+        export_json_path = os.path.join(output_folder_artifacts, 'num_values_for_node_embedding.json')
+        with open(export_json_path, 'w') as f:
+            json.dump(num_values_for_node_embedding, f)
+
+        #TODO: retornar els valors únics de cada columna per a passar-els al NodeEmbedding
 
         # Debugging information
         print(f"[DEBUG] Unique categories count: {num_categories}")
@@ -160,18 +189,6 @@ class SessionGraphEmbeddingsDataset(Dataset):
         self.sessions = self.data['user_session'].unique()
         print(f"[INFO] Found {len(self.sessions)} unique sessions.")
 
-        # Step 10: Declare embeddings model
-        print("[INFO] Declaring embeddings for categorical features...")
-        # Here we create a pre-trained embedding model for categorical features.
-
-        self.embedding_model = NodeEmbedding(
-            num_categories,
-            num_sub_categories,
-            num_elements,
-            num_brands,
-            embedding_dim)
-        print("[INFO] Declared embeddings.")
-
         self.transform = transform
         print("[INFO] SessionGraphDataset initialization complete.")
 
@@ -190,39 +207,37 @@ class SessionGraphEmbeddingsDataset(Dataset):
         session_id = self.sessions[idx]
         session_data = self.data[self.data['user_session'] == session_id]
 
-
-        #ENLLOC DE CRIDAR A UN EMBEDDING, crear un diccionari de category, sub_category, element, brand
-        # Pass data through the embedding model during the forward pass
-        # category_embeddings = self.embedding_model.forward(
-        #     torch.tensor(session_data['category'].values, dtype=torch.long),
-        #     torch.tensor(session_data['sub_category'].values, dtype=torch.long),
-        #     torch.tensor(session_data['element'].values, dtype=torch.long),
-        #     torch.tensor(session_data['brand'].values, dtype=torch.long),
-        # )
-        labels_dict = {
+        # Encode labels
+        encoded_labels_dict = {
             'category': torch.tensor(session_data['category'].values, dtype=torch.long),
             'sub_category': torch.tensor(session_data['sub_category'].values, dtype=torch.long),
             'element': torch.tensor(session_data['element'].values, dtype=torch.long),
             'brand': torch.tensor(session_data['brand'].values, dtype=torch.long)
         }
-        # Directly pass the embeddings to the graph creation function
-        return self._get_graph(session_data, session_id, labels_dict)
+        # Directly pass the encoded labels to the graph creation function
+        return self._get_graph(session_data, session_id, encoded_labels_dict)
 
-    def _get_graph(self, session_data, session_id, labels_dict):
+    def _get_graph(self, session_data, session_id, encoded_labels_dict):
         """
         Converts session interactions into a graph.
         Args:
             session_data: Filtered session DataFrame for a specific session.
             session_id: The ID of the session being processed.
-            labels_dict: Encoded labels for the session data.
+            encoded_labels_dict: Encoded labels for the session data.
         """
         # print(f"[DEBUG] Building graph for session: {session_id}")
 
-        #TODO: NO AFEGIR el últim produc_if_global de la sessio al graph.
-
         # Map product IDs to node indices
-        node_ids = session_data['product_id'].astype('category')
+        #TODO: NO AFEGIR el últim product_id_global de la sessio al graph.
+        # Do not add the last product_id of the session to the graph.
+        #TODO: passar els product_ids dels productes.
+
+        #TODO: Preguntar a l'oscar si és suficient amb passar els product_ids originals.
+        product_id_global = session_data['product_id']
+
+        node_ids = product_id_global.astype('category')
         unique_products = node_ids.cat.categories
+
         node_map = {pid: i for i, pid in enumerate(unique_products)}
 
         # Building edge list (temporal order + bidirectional edges)
@@ -238,30 +253,35 @@ class SessionGraphEmbeddingsDataset(Dataset):
 
         edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
 
-        # Aggregate embeddings by node
-        category_embeddings = category_embeddings[:len(unique_products)]  # Match number of unique products
-        price_tensor = torch.tensor(
-            session_data.groupby('product_id')['price'].first().values, dtype=torch.float
+        # TODO: passar els product_ids dels productes.
+        # Features (product_id_global)
+        #TODO: preguntar a Oscar si es així com s'han de passar, o s'ha de fer alguna transformació adicional al product_id_global.
+        product_id_global_tensor = torch.tensor(
+            product_id_global[:-1].values, dtype=torch.long
         ).unsqueeze(1)
 
-        # Ensure tensor sizes match
-        assert category_embeddings.size(0) == price_tensor.size(0), "Node counts do not match!"
-        #TODO treure embeddings
-        # Concatenate embeddings and price
-        # x = torch.cat([category_embeddings, price_tensor], dim=1)
-        price_tensor = price_tensor
+
+        # Features (price)
+        price_tensor = torch.tensor(
+            session_data[:-1].groupby('product_id')['price'].first().values, dtype=torch.float
+        ).unsqueeze(1)
+
         # Target (the last product ID to predict)
         target_product_id_global = session_data.iloc[-1]['product_id']
         y = torch.tensor(node_map[target_product_id_global], dtype=torch.long)
-        
-        #TODO: passar els product_ids dels productes.
-        product_id_global_tensor = ...
-        
-
-        #TODO:quedarnos amb sesisons que hi hagin 2 o mé s visualitzacion, més adalt.
 
         # PyG graph object
-        graph = Data(price_tensor=price_tensor, product_id_global_tensor=product_id_global_tensor, edge_index=edge_index, y=y, session_id=session_id, category=labels_dict['category'], sub_category=labels_dict['sub_category'], element=labels_dict['element'], brand=labels_dict['brand'])
+        graph = Data(price_tensor=price_tensor,
+                     product_id_global_tensor=product_id_global_tensor,
+                     edge_index=edge_index,
+                     y=y,
+                     session_id=session_id,
+                     category=encoded_labels_dict['category'],
+                     sub_category=encoded_labels_dict['sub_category'],
+                     element=encoded_labels_dict['element'],
+                     brand=encoded_labels_dict['brand'],
+                     num_nodes=len(unique_products)
+                     )
 
         if self.transform:
             graph = self.transform(graph)
