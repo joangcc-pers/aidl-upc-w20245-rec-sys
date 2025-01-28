5e973145eeed095248dd646ea30e1d99b69b4445
joangccantero <joan.gonzalez-condecantero@vistaprint.com>
2025-01-28 17:21:04 +0100

    :sparkles: run_experiment escript cleans and trains kaggle dataset using sr-gnn architecture

--- models/sr_gnn.py
+++ models/sr_gnn.py
@@ -1,32 +1,28 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import MessagePassing, global_mean_pool
-from torch_geometric.data import Data
 
 class SR_GNN(nn.Module):
     def __init__(
-        self, 
-        num_items,  #number of products in the dataset 
-        embedding_dim=100,  # the paper specifies 100 as the dimensionality for latent vectors
+        self,
         hidden_dim=100,
-        num_iterations=1  # number of 'hops', defaults to 1 as indicated in the official implementation https://github.com/CRIPAC-DIG/SR-GNN/blob/master/pytorch_code/main.py
+        num_iterations=1,  # number of 'hops', defaults to 1 as indicated in the official implementation https://github.com/CRIPAC-DIG/SR-GNN/blob/master/pytorch_code/main.py
+        num_items=None
         ):
         super(SR_GNN, self).__init__()
         
-        # Learnable embedding layer for the products in the dataset.
-        # We may need to adapt this layer depending on the final implementation choice on how to represent the different attributes of a product
-        self.item_embedding = nn.Embedding(num_items, embedding_dim) 
-        
-        self.hidden_dim=hidden_dim 
+        self.hidden_dim=hidden_dim
+        self.num_items = num_items
         self.num_iterations=num_iterations 
         
         # GGNN Layer
-        self.gnn_layer = GRUGraphLayer(embedding_dim, hidden_dim, num_iterations)
+        self.gnn_layer = GRUGraphLayer(hidden_dim, hidden_dim, num_iterations)
         
         # TODO Add Attention layer
         
-        # The linear layer maps each session embedding (final hidden state) to score for each product (num_items)
+        # The linear layer maps each session embedding (final hidden state) to score for each product (num_items). We would do nn.Linear(hidden_dim, hidden_dim in case we want to use the embedding of the graph as an input to other steps, such as an attention mechanism or an explicit calculus of similuted with the items)
+        # That is, doing nn.Linear(hidden_dim, hidden_dim) would allow us to calculate scores as similitudes (dot product) between the graph embedding and the item embeddings
+        # We opt for nn.Linear(hidden_dim, num_items) as we "just" need to produce scores for each item, an our num_items quantity is fixed and want to predict explictly the probability of each item.
         self.fc = nn.Linear(hidden_dim, num_items)
         
     def forward(
@@ -34,7 +30,7 @@ class SR_GNN(nn.Module):
         data # python geometry object containting data.x (item indices) and data.edge_index (edges)
         ):
         
-        item_embeddings = self.item_embedding(data.x) # Shape: (num_items, embedding_dim)
+        item_embeddings = data.x  # (num_items, embedding_dim) # Shape: (num_items, embedding_dim)
         
         # Pass item embeddings through the ggnn
         item_embeddings = self.gnn_layer(item_embeddings, data.edge_index) # Shape: (num_items, hidden_dim)
@@ -42,7 +38,7 @@ class SR_GNN(nn.Module):
         # TODO replace with attention mechanism
         graph_embeddings = global_mean_pool(item_embeddings, data.batch)  # Shape: (batch_size, hidden_dim)
         
-        scores = self.fc(graph_embeddings) # Shape (num_items,)
+        scores = self.fc(graph_embeddings) # Shape (batch_size, num_items)
         
         return scores
     
