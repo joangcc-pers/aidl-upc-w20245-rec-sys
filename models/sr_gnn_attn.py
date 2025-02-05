76675f212d2486560bd2a3dde2b4a80c8d183952
Xavi Rodriguez <xavi.rodriguez@ocado.com>
2025-02-05 19:51:30 +0100

    removing hardcoded values in training scripts

--- models/sr_gnn_attn.py
+++ models/sr_gnn_attn.py
@@ -19,8 +19,6 @@ class SR_GNN_attn(nn.Module):
         ):
         super(SR_GNN_attn, self).__init__()
         
-        #TODO: Iniciar la classe node_embedding i passar-li els paràmetres necessaris
-
         self.node_embedding = NodeEmbedding(num_categories, num_sub_categories, num_elements, num_brands, num_items, embedding_dim)
 
         self.hidden_dim=hidden_dim
@@ -28,10 +26,9 @@ class SR_GNN_attn(nn.Module):
         self.num_iterations=num_iterations 
         
         # GGNN Layer
-        #NOTA: 5 ja que passem 5 embeddings (category, sub_category, elements, brand i product_id). S'ha canviat a 5 ja que passarem el product id a embedding i no el passarem com a tensor, de 2 a 1 perque nomes passarem preu com a tensor, i no preu I product_id
+        # 5*embedding_dim + 1 (price tensor)
         self.gnn_layer = GRUGraphLayer(5 * embedding_dim + 1, hidden_dim, num_iterations)
-
-        # Attention
+        
         # Attention layers
         self.attention_fc = nn.Linear(hidden_dim, hidden_dim)
         self.attention_score_fc = nn.Linear(hidden_dim, 1)
@@ -39,43 +36,17 @@ class SR_GNN_attn(nn.Module):
         # The linear layer maps each session embedding (final hidden state) to score for each product (num_items). 
         self.fc = nn.Linear(hidden_dim, num_items)
         
-    def forward(
-        self, 
-        data # python geometry object containting data.x (item indices) and data.edge_index (edges)
-        ):
-        
+    def forward(self, data):
         embedding = self.node_embedding.forward(data.category, data.sub_category, data.element, data.brand, data.product_id_remapped)
-
-        # Print shapes for debugging
-        # print(f"price_tensor shape: {data.price_tensor.shape}")
-        # print(f"embedding shape: {embedding.shape}")
         
         # Concatenate item embeddings with price tensor
         item_embeddings = torch.cat([data.price_tensor,
                                     embedding
                                      ], dim=1) # Shape: (num_items, embedding_dim)
-        # print(f"item_embeddings shape: {item_embeddings.shape}")
+        
         # Pass item embeddings through the gnn
         item_embeddings_gnn = self.gnn_layer(item_embeddings, data.edge_index) # Shape: (num_items, hidden_dim)
-        # print(f"item_embeddings_gnn shape: {item_embeddings_gnn.shape}")
-
-        # print(f"item_embeddings_gnn.shape: {item_embeddings_gnn.shape}")  # Esperado: (N, hidden_dim)
-        # print(f"data.batch.shape: {data.batch.shape}")  # Esperado: (N,)
-        # print(f"data.batch unique values: {data.batch.unique()}")  # Debería ser el número de sesiones
-        # print(f"Edge index shape: {data.edge_index.shape}")  # Ver si hay nodos desconectados
-
-        # connected_nodes = torch.unique(data.edge_index)  # Nodos que aparecen en los edges
-        # all_nodes = torch.arange(item_embeddings_gnn.shape[0], device=item_embeddings_gnn.device)
-        # isolated_nodes = torch.tensor([n for n in all_nodes if n not in connected_nodes])
-
-        # print(f"Nodos aislados: {isolated_nodes}")
-        # print(f"Cantidad de nodos aislados: {len(isolated_nodes)}")
-
-        # session_counts = torch.bincount(data.batch)
-        # print("Distribución de nodos por sesión:", session_counts.unique(return_counts=True))
-
-        # Embedding representing the last clicked index for each session in the batch 
-        # target_embedding = self.global_product_embeddings(data.y) # Shape (batch_size, hidden_dim)
+        
         graph_embeddings = self.attention_mechanism(item_embeddings_gnn, data.batch)
         
         scores = self.fc(graph_embeddings) # Shape (batch_size, num_items)
@@ -132,12 +103,3 @@ class GRUGraphLayer(MessagePassing):
 
     def message(self, x_j):
         return x_j
-        
-        
-        
-# DATA
-# Training                              Loss
-# Session 1-2-3-4       last click 4    data.y = target 5
-
-# Inference
-# Session 1-2-3-4-5     last click 5    target <predicted item>
