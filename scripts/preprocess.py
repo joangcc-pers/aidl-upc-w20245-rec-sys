b4f92854f1d64b7473545f97bae0647cae2c3d28
joangccantero <joan.gonzalez-condecantero@vistaprint.com>
2025-02-07 07:50:01 +0100

    :construction: Integrating validation and test in run_experiment.py current workflow

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,5 +1,5 @@
 from scripts.preprocessing_scripts.preprocess_graph_with_embeddings import preprocess_graph_with_embeddings
-from scripts.preprocessing_scripts.preprocess_sr_gnn import preprocess_sr_gnn
+# from scripts.preprocessing_scripts.preprocess_sr_gnn import preprocess_sr_gnn
 
 def preprocess_data(model_name, input_folder_path, output_folder_artifacts, preprocessing_params):
     
@@ -10,8 +10,8 @@ def preprocess_data(model_name, input_folder_path, output_folder_artifacts, prep
         dataloader = preprocess_graph_with_embeddings(input_folder_path, output_folder_artifacts, preprocessing_params)
     elif model_name == "graph_with_embeddings_and_attention":
         dataloader = preprocess_graph_with_embeddings(input_folder_path, output_folder_artifacts, preprocessing_params)
-    elif model_name in {"sr_gnn", "sr_gnn_test_mockup"}:
-        dataloader = preprocess_sr_gnn(input_folder_path, preprocessing_params)
+    # elif model_name in {"sr_gnn", "sr_gnn_test_mockup"}:
+    #     dataloader = preprocess_sr_gnn(input_folder_path, preprocessing_params)
     else:
         raise ValueError(f"Unsupported model name: {model_name}")
 
--- scripts/preprocessing_scripts/preprocess_sr_gnn.py
+++ scripts/preprocessing_scripts/preprocess_sr_gnn.py
@@ -1,25 +1,29 @@
-import os
-import torch.optim as optim
-import torch.nn as nn
-import json
+##########################
+##### DEPRECATE CODE #####
+##########################
 
-from scripts.preprocessing_scripts.sr_gnn_dataset_test import SessionDataset
-from torch_geometric.loader import DataLoader
+# import os
+# import torch.optim as optim
+# import torch.nn as nn
+# import json
 
-def preprocess_sr_gnn(input_folder_path, preprocessing_params):
-    """Preprocessing pipeline for SR GNN."""
-    print("Applying preprocessing for SR-GNN...")
-    # Initialising random session data
-    file_path = os.path.abspath("./data/raw/sr_gnn_mockup/random_sessions.json")
-    sessions = []
+# from scripts.preprocessing_scripts.sr_gnn_dataset_test import SessionDataset
+# from torch_geometric.loader import DataLoader
 
-    with open(file_path, "r") as f:
-        sessions = json.load(f)
-    print(f"Loaded {len(sessions)} sessions.")
+# def preprocess_sr_gnn(input_folder_path, preprocessing_params):
+#     """Preprocessing pipeline for SR GNN."""
+#     print("Applying preprocessing for SR-GNN...")
+#     # Initialising random session data
+#     file_path = os.path.abspath("./data/raw/sr_gnn_mockup/random_sessions.json")
+#     sessions = []
 
-    num_items = 100  # Total number of unique items in the catalog
-    embedding_dim = preprocessing_params.get("embedding_dim",100)
+#     with open(file_path, "r") as f:
+#         sessions = json.load(f)
+#     print(f"Loaded {len(sessions)} sessions.")
 
-    dataset = SessionDataset(sessions, num_items, embedding_dim=embedding_dim)
-    dataloader = DataLoader(dataset, batch_size=100, shuffle=preprocessing_params["shuffle"])
-    return dataloader
+#     num_items = 100  # Total number of unique items in the catalog
+#     embedding_dim = preprocessing_params.get("embedding_dim",100)
+
+#     dataset = SessionDataset(sessions, num_items, embedding_dim=embedding_dim)
+#     dataloader = DataLoader(dataset, batch_size=100, shuffle=preprocessing_params["shuffle"])
+#     return dataloader
