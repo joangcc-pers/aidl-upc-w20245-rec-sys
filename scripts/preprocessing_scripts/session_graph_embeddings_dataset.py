669ae052ff489baae417a23d58d64835daa5f8f0
joangccantero <joan.gonzalez-condecantero@vistaprint.com>
2025-02-07 13:44:12 +0100

    :bug: Fix temporal splitting and continue cleaning repo

--- scripts/preprocessing_scripts/session_graph_embeddings_dataset.py
+++ scripts/preprocessing_scripts/session_graph_embeddings_dataset.py
@@ -305,7 +305,8 @@ class SessionGraphEmbeddingsDataset(Dataset):
                      element=encoded_labels_dict['element'][:-1],
                      brand=encoded_labels_dict['brand'][:-1],
                      product_id_remapped=torch.tensor(session_data['product_id_remapped'][:-1].values, dtype=torch.long),
-                     num_nodes = len(session_data_without_last)
+                     num_nodes = len(session_data_without_last),
+                     time = session_data['event_time'].min()
                      )
 
         if self.transform:
